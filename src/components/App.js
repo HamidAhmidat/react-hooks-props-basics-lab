8829af276eefba416d97063b6476c2a7e1761910
hamid <ahmidatabdelhamid@gmail.com>
2023-09-09 19:49:19 +0100

    Completed assignment

--- src/components/App.js
+++ src/components/App.js
@@ -6,13 +6,20 @@ import About from "./About";
 // pass this data down as props to the child component(s) that need it!
 import user from "../data/user";
 console.log(user);
+// const a = user.links.linkedin;
+// const b = user.links.github;
 
 function App() {
   return (
     <div>
       <NavBar />
-      <Home />
-      <About />
+      <Home name="Liza" city="New York" color="firebrick" />
+      <About
+        bio="I made this!"
+        github={user.links.github}
+        linkedin={user.links.linkedin}
+      />
+      '
     </div>
   );
 }
